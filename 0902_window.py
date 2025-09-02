00fd9b7a0af19377aa5e67c17c0185afb6e73bca
jhhyoo <veb01-wi250014@sankogakuen.jp>
2025-09-02 09:33:01 +0900

    Update 0902_window.py

--- 0902_window.py
+++ 0902_window.py
@@ -8,4 +8,3 @@ canvas = tkinter.Canvas(width=WINDOW_SIZE, height=WINDOW_SIZE/2, bg="pink")
 canvas.create_text(WINDOW_SIZE/2, WINDOW_SIZE/4, text="Python", font=("Times New Roman", 40))
 canvas.pack()
 root.mainloop()
-
